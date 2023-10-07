bb3111871532142e04c717e26d0ccc3b72c9e26c
nagasawa ami <amiponcha@gmail.com>
2023-10-07 18:14:22 +0900

    変更

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contacts;
+
+class ContactController extends Controller
+{
+    //トップ表示
+    public function index()
+    {
+        return view('index');
+    }
+
+    //入力フォームのデータの受け取り
+    public function confirm(Request $request)
+    {
+        $contact = $request->only(['name'. 'email', 'tel', 'content']);
+        return view('confirm', compact('contact'));
+    }
+
+
+    //データ受け取りのデータの保存処理
+    public function store(Request $request)
+    {
+        $contact = $request->only(['name'. 'email', 'tel', 'content']);
+        Contact::create($contact);
+        return view('thanks');
+    }
+
+
+
+
+}
